8f7e8bd24c058f11512f06d097f41fbd594615c9
coookiesMonster <srisamaipatid@gmail.com>
2024-03-04 09:23:29 +0100

    Dependency and stuff ( mainly express and nodemon)

---
+++ core_file/index.js
@@ -0,0 +1,15 @@
+/* Express */
+const express = require('express');
+const app = express();
+
+
+
+
+/* Express */
+app.listen(3456, err=> {
+    if(err) return console.log(err);
+    console.log("lyssnar på 3456");   
+});
+
+
+/* Routes */
